4079c1992bb1d87db643c81da6801b65c637e570
SimiRiza <simiriza14@gmail.com>
2026-07-24 21:13:16 +0530

    feat(auth): implement user registration with validation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Patterns from "./pages/Patterns";
 import Problems from "./pages/Problems";
+import Registration from "./pages/Registration";
 
 function App() {
-
     return (
-
         <Routes>
-
             <Route path="/" element={<Home />} />
-
             <Route path="/patterns/:sheetId" element={<Patterns />} />
-
             <Route path="/problems/:patternId" element={<Problems />} />
-
+            <Route path="/login" element={<Registration />} />
         </Routes>
-
     );
-
 }
 
 export default App;
